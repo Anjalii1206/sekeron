44f8dc13fb5cbf8b24d84739b3b52b318a88203b
unknown <vermanjali721@gmail.com>
2024-08-17 16:12:01 +0530

    some changes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardBody, Text, Heading, Box, Flex, Image } from "@chakra-ui/react";
+import { Card, CardBody, Text, Heading, Box, Flex, Image, } from "@chakra-ui/react";
 //import '../App.css';
 
 function Weather({ data }) {
@@ -14,29 +14,50 @@ function Weather({ data }) {
     let bgImg = "/Images/default.jpg";
     let w_icon = "/weather_icons/default.png";
 
-    if ( current.condition.text === "Sunny" || current.condition.text === "Clear" ) {
-      bgImg = "/Images/sunny.jpg";
-      w_icon = "/weather_icons/sunny.png";
-    } else if ( current.condition.text === "Cloudy" || current.condition.text === "Partly cloudy" ) {
-      bgImg = "/Images/cloudy.jpg";
-      w_icon = "/weather_icons/clouds.png";
-    } else if ( current.condition.text === "Rain" || current.condition.text === "Light Rain" || current.condition.text === "Patchy rain nearby" || current.condition.text === "Light rain shower" ) {
-      bgImg = "/Images/rainy.jpg";
-      w_icon = "/weather_icons/rain.png";
-    } else if (current.condition.text === "Snow") {
-      bgImg = "/Images/snowy.jpg";
-      w_icon = "/weather_icons/snow.png";
-    } else if (current.condition.text === "Mist") {
-      bgImg = "/Images/mist.jpg";
-      w_icon = "/weather_icons/mist.png";
-    } else if (current.condition.text === "Windy") {
-      bgImg = "/Images/windy.jpg";
-      w_icon = "/weather_icons/wind.png";
-    } else if (current.condition.text === "Haze") {
-      bgImg = "/Images/hazy.jpg";
-      w_icon = "/weather_icons/haze.png";
-    } else {
-      console.log("Condition not matched:", current.condition.text);
+    switch (current.condition.text) {
+      case "Sunny":
+      case "Clear":
+        bgImg = "/Images/sunny.jpg";
+        w_icon = "/weather_icons/sunny.png";
+        break;
+
+      case "Cloudy":
+      case "Partly cloudy":
+        bgImg = "/Images/cloudy.jpg";
+        w_icon = "/weather_icons/clouds.png";
+        break;
+
+      case "Rain":
+      case "Light Rain":
+      case "Patchy rain nearby":
+      case "Light rain shower":
+        bgImg = "/Images/rainy.jpg";
+        w_icon = "/weather_icons/rain.png";
+        break;
+
+      case "Snow":
+        bgImg = "/Images/snowy.jpg";
+        w_icon = "/weather_icons/snow.png";
+        break;
+
+      case "Mist":
+        bgImg = "/Images/mist.jpg";
+        w_icon = "/weather_icons/mist.png";
+        break;
+
+      case "Windy":
+        bgImg = "/Images/windy.jpg";
+        w_icon = "/weather_icons/wind.png";
+        break;
+
+      case "Haze":
+        bgImg = "/Images/hazy.jpg";
+        w_icon = "/weather_icons/haze.png";
+        break;
+
+      default:
+        console.log("Condition not matched:", current.condition.text);
+        break;
     }
 
     setWIcon(w_icon);
@@ -58,21 +79,21 @@ function Weather({ data }) {
       <CardBody>
         <Flex flexDirection={{ base: "column", md: "row" }} justify="space-between" alignItems="center" marginBottom="20px" >
           <Box>
-            <Heading as="h3" size={{ base: "lg", md: "2xl" }}> {location.name} </Heading>
+            <Heading as="h3" size={{ base: "lg", md: "2xl" }}> {" "} {location.name}{" "} </Heading>
             <Text fontSize={{ base: "sm", md: "md" }}>{location.region}</Text>
             <Text fontSize={{ base: "sm", md: "md" }}>{location.country}</Text>
           </Box>
           <Box textAlign="right" p={1}>
-            <Text fontSize={{ base: "sm", md: "lg" }} fontWeight="medium"> {formattedDate} </Text>
-            <Text fontSize={{ base: "sm", md: "lg" }} fontWeight="medium"> {day} </Text>
+            <Text fontSize={{ base: "sm", md: "lg" }} fontWeight="medium"> {" "} {formattedDate}{" "} </Text>
+            <Text fontSize={{ base: "sm", md: "lg" }} fontWeight="medium"> {" "} {day}{" "} </Text>
           </Box>
         </Flex>
 
         <Flex flexDirection={{ base: "column", md: "row" }} alignItems="center" justify="space-between" >
-          <Text fontSize={{ base: "3xl", md: "6xl" }} fontWeight="medium" mb={{ base: "2px" }} > {current.temp_c}°C </Text>
+          <Text fontSize={{ base: "3xl", md: "6xl" }} fontWeight="medium" mb={{ base: "2px" }} > {" "} {current.temp_c}°C{" "} </Text>
           <Box textAlign="center" mt={{ base: "1px", md: "0" }} ml={{ base: "0", md: "20px" }} >
             <Image src={wIcon} alt={current.condition.text} boxSize={{ base: "40px", md: "60px" }} marginBottom="10px" />
-            <Text fontSize={{ base: "md", md: "lg" }} fontWeight="medium" marginBottom="10px" > {current.condition.text} </Text>
+            <Text fontSize={{ base: "md", md: "lg" }} fontWeight="medium" marginBottom="10px" > {" "} {current.condition.text}{" "} </Text>
           </Box>
         </Flex>
 
